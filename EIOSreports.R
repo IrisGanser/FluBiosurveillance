17a5e951c1f3ddc3a537853413e389439ff93cae
Iris Ganser <iris.ganser@mail.mcgill.ca>
2020-02-03 17:21:20 -0500

    added barplot for total event counts

--- EIOSreports.R
+++ EIOSreports.R
@@ -29,7 +29,8 @@ country_list <- levels(FluNet_data$Country)
 
 
 # load EIOS data
-EIOSreports <- read.csv("D:/Dokumente (D)/McGill/Thesis/iris_flu_analysis/EIOSreports.csv", comment.char="#", stringsAsFactors=FALSE)
+EIOSreports <- read.csv("D:/Dokumente (D)/McGill/Thesis/SurveillanceData/iris_flu_analysis/EIOSreports.csv", 
+                        comment.char="#", stringsAsFactors=FALSE)
 EIOSreports <- EIOSreports %>% rename(Country = mentionedCountries, CountryIso2 = mentionedCountriesIso2) %>% 
   select(-c(X, translatedFullText, translatedTitle))
 
@@ -50,6 +51,19 @@ EIOSreports$fetchDate <- as.POSIXct(EIOSreports$fetchDate, format="%Y-%m-%d %H:%
 EIOSreports$importDate <- gsub("Z", "", EIOSreports$importDate) 
 EIOSreports$importDate <- gsub("T", " ", EIOSreports$importDate)
 EIOSreports$importDate <- as.POSIXct(EIOSreports$importDate, format="%Y-%m-%d %H:%M:%S")
+
+
+# barplot of healthmap event counts in total over 5 years
+ggplot(EIOSreports, aes(x = Country)) + 
+  geom_bar(fill = "darkorange3") + 
+  geom_text(aes(label=..count..), stat="count", position = position_stack(0)) + 
+  labs(title = "Total number of EIOS events from November 2017 - November 2019", x = "") + 
+  coord_flip() +
+  scale_x_discrete(limits = rev(levels(EIOSreports$Country))) +
+  scale_y_continuous(breaks = c(0, 5000, 10000, 15000, 20000, 25000), limits = c(0, 28000))
+
+#ggsave("EIOS total counts.jpeg")
+
 
 
 # summarize into weekly data
@@ -76,11 +90,11 @@ for (i in seq_along(country_list)) {
     ylab(NULL) +
     theme(strip.background = element_blank(), strip.placement = "outside") + 
     labs(title = paste("Comparison of EIOS and FluNet counts for", country_list[i], sep = " ")) +
-    scale_x_datetime(date_labels = "%b %Y", date_breaks = "12 months") + 
+    scale_x_datetime(date_labels = "%b %Y", date_breaks = "6 months") + 
     xlab("")
   
   print(plot)
-    #ggsave(plot = plot, file = paste("WHO HM comparison", country_list[i], ".jpeg", sep=' '))
+  #ggsave(plot = plot, file = paste("EIOS FluNet comparison", country_list[i], ".jpeg", sep=' '))
 }
 
 
